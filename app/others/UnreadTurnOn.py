25b3ea65fa065eae32d75add305395ba33c57f1d
s.szulcs1 <s.szulc@fundacjadziewieciubraci.pl>
2023-06-29 23:46:21 +0200

    temporally turn off

--- app/others/UnreadTurnOn.py
+++ app/others/UnreadTurnOn.py
@@ -1,7 +1,8 @@
 from flask_restful import Resource, reqparse
 from app.Data import Mongo
-
+from app.common import get_message
 
 class UnreadTurnOn(Resource):
     def post(self):
-        Mongo.update("users", {"uid": 1}, {"$set": {"Flags.unread_notify": True}})
+        return get_message("Funkcja tymczasowo wyłączona"), 200
+        # Mongo.update("users", {"uid": 1}, {"$set": {"Flags.unread_notify": True}})
